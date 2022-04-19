dc7cbdf0940d30d51ded8f754be31c53966dff9a
Gabriella Agathon <gjstimac@mtu.edu>
2022-04-19 18:27:46 +0000

    Messing around with some cpp

--- cpp/stringgen.cpp
+++ cpp/stringgen.cpp
@@ -2,15 +2,18 @@
 #include <string>
 using namespace std;
 
-int main(){
-    int starter = 5; //Overall length of string
-    char startChar = 65; //Starting character value ("A")
+int main(int argc, char *argv[]){
+    int starter = (signed int) *argv[1]; //Overall length of string
+    char startChar = *argv[2]; //Starting character value ("A")
     string output = "";
+    string debug = "\nstarting char: " + startChar;
+    string debug2 = "\nstarting pos:" + starter;
+    cout << debug << debug2 << "\n";
 
     for(int i = 0; i < starter; i++){
         output = startChar;
         cout << output;
-        startChar++;
-    }
+        
+    } 
     return 0;
 }
