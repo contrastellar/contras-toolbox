34203bdf3cb29bca3d2ea5468e522fb6b30fe293
Gabriella Agathon <gjstimac@mtu.edu>
2022-06-10 18:18:09 +0000

    created HTML code files

---
+++ python/html-codes.py
@@ -0,0 +1,5 @@
+# Script to fetch an html code and then return the associated message
+# author: Gabriella Agathon (contrastellar), 2022
+# based upon RFC 7231 (fetched 06/10/2022) https://datatracker.ietf.org/doc/html/rfc7231#section-6
+
+
