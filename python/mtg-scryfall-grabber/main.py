77536ab35f3a1f93d64a5e9329fab07122f241e0
Gabriella Agathon <gjstimac@mtu.edu>
2023-03-01 19:48:29 -0500

    First api requests from the scryfall API work

--- python/mtg-scryfall-grabber/main.py
+++ python/mtg-scryfall-grabber/main.py
@@ -1,6 +1,6 @@
 # https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Aone&unique=prints
 
-import os, requests, json
+import os, requests, json, argparse
 
 # Return a string object of the json passed in via obj
 # !! (Likely) Unused
@@ -8,7 +8,28 @@ def jsonParse(obj):
     text = json.dumps(obj, sort_keys=True, indent=3)
     return text
 
+def main():
 
-script_dir = os.path.abspath( os.path.dirname( __file__ ) )
+    # Current dir the script is running in, can be useful for debugging
+    script_dir = os.path.abspath( os.path.dirname( __file__ ) )
+
+    description = "Script to pull card info from api.scryfall.com based on specific set"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('set', metavar='set') #set to pull card data from
+    parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
+
+    # Parse arguments
+    args = parser.parse_args()
+    verboseSetting = bool(args.verbose)
+    user_set = str(args.set)
+
+    if(verboseSetting): print("\n"+script_dir)
+
+    response_URL = "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Aone&unique=prints"
+    responseData = requests.get(response_URL)
+
+    if(verboseSetting): print("Response code: " + str(responseData.status_code) + "\n")
 
 
+if __name__ == "__main__":
+    main()
